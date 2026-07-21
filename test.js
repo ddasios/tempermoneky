3d1dea786c9306195e0ea595905f96821f40e3eb
Dennis Dasios <ddasios@gmail.com>
2026-07-21 09:55:45 +0300

    wip

---
+++ test.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Order Auto Fill
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Test
+// @match        https://dirtynoise.gr*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('works');
+    // Your code here...
+})();
